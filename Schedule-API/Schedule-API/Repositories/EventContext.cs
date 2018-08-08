40d377d854c44cff72ab728538d5b45433d36e71
Mate Elek <elekamate@gmail.com>
2018-08-08 14:25:42 +0200

    Add(DB): Event + EventTemplate tables added.

---
+++ Schedule-API/Schedule-API/Repositories/EventContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ScheduleAPI.Models
+{
+    public partial class EventContext : DbContext
+    {
+        public EventContext()
+        {
+        }
+
+        public EventContext(DbContextOptions<EventContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Events> Events { get; set; }
+        public virtual DbSet<EventTemplates> EventTemplates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MarvinDB;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Events>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Type).HasMaxLength(10);
+
+                entity.HasOne(d => d.TemplateNavigation)
+                    .WithMany(p => p.Events)
+                    .HasForeignKey(d => d.Template)
+                    .HasConstraintName("FK_Events_ToEventTemplates");
+            });
+
+            modelBuilder.Entity<EventTemplates>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
--- Schedule-API/Schedule-API/Repositories/EventRepository.cs
+++ Schedule-API/Schedule-API/Repositories/EventRepository.cs
@@ -7,40 +7,40 @@ namespace ScheduleAPI.Repositories
 {
     public class EventRepository
     {
-        private ContextName eventContextObj;
+        //private ContextName eventContextObj;
 
-        public EventRepository(ContextName ContextObj)
-        {
-            this.eventContextObj = ContextObj;
-        }
+        //public EventRepository(ContextName ContextObj)
+        //{
+        //    this.eventContextObj = ContextObj;
+        //}
 
-        public void Create(tableclass eventClassObj)
-        {
-            eventContextObj.Add(eventClassObj);
-            eventContextObj.SaveChanges();
-        }
+        //public void Create(tableclass eventClassObj)
+        //{
+        //    eventContextObj.Add(eventClassObj);
+        //    eventContextObj.SaveChanges();
+        //}
 
-        public List<tableclass> Read()
-        {
-            return eventContextObj.tableName.ToList();
-        }
+        //public List<tableclass> Read()
+        //{
+        //    return eventContextObj.tableName.ToList();
+        //}
 
-        public void Update(tableclass classObj)
-        {
-            eventContextObj.Update(classObj);
-            eventContextObj.SaveChanges();
-        }
+        //public void Update(tableclass classObj)
+        //{
+        //    eventContextObj.Update(classObj);
+        //    eventContextObj.SaveChanges();
+        //}
 
-        public void Delete(int id)
-        {
-            var removable = eventContextObj.tableName.ToList().FirstOrDefault(x => x.Id == id);
-            eventContextObj.Remove(removable);
-            eventContextObj.SaveChanges();
-        }
+        //public void Delete(int id)
+        //{
+        //    var removable = eventContextObj.tableName.ToList().FirstOrDefault(x => x.Id == id);
+        //    eventContextObj.Remove(removable);
+        //    eventContextObj.SaveChanges();
+        //}
 
-        public tableclass GetTodoById(long id)
-        {
-            return eventContextObj.tableName.ToList().FirstOrDefault(x => x.Id == id);
-        }
+        //public tableclass GetTodoById(long id)
+        //{
+        //    return eventContextObj.tableName.ToList().FirstOrDefault(x => x.Id == id);
+        //}
     }
 }
