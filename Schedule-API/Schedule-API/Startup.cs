24d65faa209cbbdfecef304c764ae5af96cc5b60
zynori <zynori@gmail.com>
2018-08-27 16:33:15 +0200

    Feat(EventApi): add swagger

--- Schedule-API/Schedule-API/Startup.cs
+++ Schedule-API/Schedule-API/Startup.cs
@@ -7,6 +7,7 @@ using ScheduleAPI.Models;
 using ScheduleAPI.Repositories;
 using ScheduleAPI.Services;
 using ScheduleAPI.ViewModels;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Schedule_API
 {
@@ -24,13 +25,20 @@ namespace Schedule_API
         {
             string connectionStringToDoDB = Configuration.GetConnectionString("connString");
             services.AddMvc();
+
             services.AddDbContext<EventContext>(options =>
                 options.UseSqlServer(connectionStringToDoDB));
+
             services.AddTransient<EventContext>();
             services.AddTransient<EventRepository>();
             services.AddTransient<EventTemplateRepository>();
             services.AddTransient<EventService>();
             services.AddTransient<EventViewModel>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MarvinEvent", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +49,14 @@ namespace Schedule_API
                 app.UseDeveloperExceptionPage();
             }
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MarvinEvent V1");
+            });
+
+            app.UseStaticFiles();
         }
     }
 }
