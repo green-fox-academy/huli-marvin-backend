aea9f36058c5af162c8a3f9b6e851c0edf88ac75
zynori <zynori@gmail.com>
2018-08-27 13:51:30 +0200

    fixed naming

--- Member-API/MemberService/Startup.cs
+++ Member-API/MemberService/Startup.cs
@@ -71,7 +71,7 @@ namespace MemberService
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Marvin V1");
                 c.RoutePrefix = string.Empty;
             });
 
