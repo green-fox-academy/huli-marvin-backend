7ede0056711c1a3df39d5629e601e549cc90aec6
Mate Elek <elekamate@gmail.com>
2018-09-10 16:55:33 +0200

    Chore(DBSeed): Finishing the day, no progress.

--- Schedule-API/Schedule-API/Entities/EventContext.cs
+++ Schedule-API/Schedule-API/Entities/EventContext.cs
@@ -3,14 +3,23 @@ using Microsoft.Extensions.Configuration;
 
 namespace ScheduleAPI.Models
 {
-    public partial class EventContext : DbContext
+    public class EventContext : DbContext
     {
         public virtual DbSet<Event> Events { get; set; }
         public virtual DbSet<EventTemplate> EventTemplates { get; set; }
 
-        public EventContext(DbContextOptions<EventContext> options)
-            : base(options)
+        public EventContext(DbContextOptions<EventContext> options) : base(options)
         {
+        }
+
+        public void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Properties()
+                .Where(p => p.Name == p.DeclaringType.Name + "_ID")
+                .Configure(p => p.IsKey());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
