b6b212a11919a974e4609d0a749bdce44d1dec78
Mate Elek <elekamate@gmail.com>
2018-09-20 17:10:53 +0200

    Chore(MemberAPI-csproj): Repairing solution.

--- Schedule-API/ScheduleApiUnitTests/EventControllerUnitTest.cs
+++ Schedule-API/ScheduleApiUnitTests/EventControllerUnitTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using ScheduleAPI.Controllers;
 using ScheduleAPI.Models;
 using ScheduleAPI.Repositories;
@@ -12,10 +13,10 @@ namespace ScheduleApiUnitTests
 {
     public class EventControllerUnitTest
     {
-        private EventViewModel eventViewModel;
-        private EventRepository eventRepository;
-        private EventTemplateRepository eventTemplateRepository;
-        private PaginationService paginationService;
+        //private EventViewModel eventViewModel;
+        //private EventRepository eventRepository;
+        //private EventTemplateRepository eventTemplateRepository;
+        //private PaginationService paginationService;
 
         private EventController eventController;
         private readonly Event events;
@@ -23,16 +24,30 @@ namespace ScheduleApiUnitTests
         public EventControllerUnitTest()
         {
             events = new Event();
-            eventController = new EventController(eventViewModel, eventRepository, eventTemplateRepository, paginationService);
+            
         }
 
         [Fact]
         public async Task FindEvent_Should_ReturnEvent_InJson()
         {
+            eventController = new EventController(eventViewModel: null, eventRepository: null, eventTemplateRepository: null, paginationService: null);
+
+            int testSessionId = 1;
+            var mockRepo = new Mock<EventRepository>();
+            mockRepo.Setup(repo => repo.GetItemByIdAsync(testSessionId))
+                .ReturnsAsync((BrainstormSession)null);
+            var controller = new SessionController(mockRepo.Object);
+
             var result = await eventController.GetEvent(events.EventId);
             var jsonResult = result as JsonResult;
 
             Assert.IsType<JsonResult>(jsonResult);
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 2+2);
         }
     }
 }
